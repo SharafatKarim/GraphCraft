bd7498ffeb9ed32ade568fcef29f69cf7d7351c9
SharafatKarim <sharafat2004@gmail.com>
2024-03-22 16:06:43 +0600

    2 represantation

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ var cy = cytoscape({
         name: 'cose'
     },
     "elements": [
-    { "data": { "id": "a" } },
-    { "data": { "id": "b" } },
-    { "data": { "id": "c" } },
-    { "data": { "id": "d" } },
-    { "data": { "id": "e" } },
-    { "data": { "id": "f" } },
-    { "data": { "id": "1", "source": "a", "target": "d", "weight": 1 } },
-    { "data": { "id": "2", "source": "b", "target": "e", "weight": 2 } },
-    { "data": { "id": "3", "source": "c", "target": "f", "weight": 1 } },
-    { "data": { "id": "4", "source": "a", "target": "b", "weight": 1 } },
-    { "data": { "id": "5", "source": "b", "target": "c", "weight": 2 } },
-    { "data": { "id": "6", "source": "c", "target": "d", "weight": 1 } },
-    { "data": { "id": "7", "source": "d", "target": "e", "weight": 1 } },
-    { "data": { "id": "8", "source": "e", "target": "f", "weight": 2 } },
-    { "data": { "id": "9", "source": "f", "target": "a", "weight": 1 } }
-],
+        { "data": { "id": "a" } },
+        { "data": { "id": "b" } },
+        { "data": { "id": "c" } },
+        { "data": { "id": "d" } },
+        { "data": { "id": "e" } },
+        { "data": { "id": "f" } },
+        { "data": { "id": "1", "source": "a", "target": "d", "weight": 1 } },
+        { "data": { "id": "2", "source": "b", "target": "e", "weight": 2 } },
+        { "data": { "id": "3", "source": "c", "target": "f", "weight": 1 } },
+        { "data": { "id": "4", "source": "a", "target": "b", "weight": 1 } },
+        { "data": { "id": "5", "source": "b", "target": "c", "weight": 2 } },
+        { "data": { "id": "6", "source": "c", "target": "d", "weight": 1 } },
+        { "data": { "id": "7", "source": "d", "target": "e", "weight": 1 } },
+        { "data": { "id": "8", "source": "e", "target": "f", "weight": 2 } },
+        { "data": { "id": "9", "source": "f", "target": "a", "weight": 1 } }
+    ],
     style: [
         {
             selector: 'node',
@@ -54,6 +54,7 @@ document.getElementById('myButton').addEventListener('click', function () {
     console.log("length : ", length);
 
     addAdjMatTable(length, arr);
+    addLinkedTable(length, arr);
     addNode(length, arr);
 
     // cy.add([
@@ -104,22 +105,20 @@ function addAdjMatTable(n, arr) {
     var tableBody = document.createElement('TBODY');
     table.appendChild(tableBody);
 
-    for (var i = 0; i < 1; i++) {
-        var tr = document.createElement('TR');
-        tableBody.appendChild(tr);
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
 
-        for (var j = 0; j <= n; j++) {
-            if (j == 0) {
-                var th = document.createElement('TH');
-                th.appendChild(document.createTextNode('#'));
-                tr.appendChild(th);
-                continue;
-            }
+    for (var j = 0; j <= n; j++) {
+        if (j == 0) {
             var th = document.createElement('TH');
-            // th.appendChild(document.createTextNode("To " + toString(j+'a')));
-            th.appendChild(document.createTextNode("To " + String.fromCharCode(96 + j)));
+            th.appendChild(document.createTextNode('#'));
             tr.appendChild(th);
+            continue;
         }
+        var th = document.createElement('TH');
+        // th.appendChild(document.createTextNode("To " + toString(j+'a')));
+        th.appendChild(document.createTextNode("To " + String.fromCharCode(96 + j)));
+        tr.appendChild(th);
     }
 
     for (var i = 0; i < n; i++) {
@@ -135,6 +134,51 @@ function addAdjMatTable(n, arr) {
             td.appendChild(document.createTextNode(arr[i][j]));
             tr.appendChild(td);
         }
+    }
+    myTableDiv.appendChild(table);
+}
+
+function addLinkedTable(n, arr) {
+    document.getElementById('lnkText').style.visibility = 'visible';
+    var myTableDiv = document.getElementById("linkedRepTable");
+
+    if (myTableDiv.hasChildNodes()) {
+        myTableDiv.removeChild(myTableDiv.childNodes[0]);
+    }
+
+    var table = document.createElement('TABLE');
+
+    table.className = 'table table-hover';
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode('Node'));
+    tr.appendChild(th);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("Adjacency List"));
+    tr.appendChild(th);
+
+    for (var i = 0; i < n; i++) {
+        var tr = document.createElement('TR');
+        var th = document.createElement('TH');
+        // th.appendChild(document.createTextNode("From " + i));
+        th.appendChild(document.createTextNode(String.fromCharCode(96 + i + 1)));
+        tr.appendChild(th);
+        tableBody.appendChild(tr);
+        
+        var data = "";
+        for (var j = 0; j < n; j++) {
+            if (arr[i][j] != 0) {
+                data += String.fromCharCode(96 + j + 1) + " ";
+            }
+        }
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(data));
+        tr.appendChild(td);
     }
     myTableDiv.appendChild(table);
 }
