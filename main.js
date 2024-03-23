b25926a11daa5374661c7bc89a26e4dbcfde7347
SharafatKarim <sharafat2004@gmail.com>
2024-03-23 11:28:21 +0600

    warshell and a bit more

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ var cy = cytoscape({
         }
     ],
     // zoomingEnabled: false, // disable zooming
-    userZoomingEnabled: false // disable user zooming
+    // userZoomingEnabled: false // disable user zooming
 });
 
 document.getElementById('myButton').addEventListener('click', function () {
@@ -57,15 +57,12 @@ document.getElementById('myButton').addEventListener('click', function () {
     addLinkedTable(length, arr);
     addNode(length, arr);
 
-    // cy.add([
-    //     { group: 'nodes', data: { id: 'a' } },
-    //     { group: 'nodes', data: { id: 'b' } },
-    //     { group: 'edges', data: { id: 'ab', source: 'a', target: 'b' } },
-    //     { group: 'edges', data: { id: 'ba', source: 'b', target: 'a' } }
-    // ]);
+    warshall(length, arr);
+    pathMatrix(length, arr);
 });
 
 function addNode(length, arr) {
+    cy.remove(cy.elements());
     var nodes = [];
     for (var i = 0; i < length; i++) {
         nodes.push({ data: { id: String.fromCharCode(97 + i) } });
@@ -86,7 +83,7 @@ function addNode(length, arr) {
 }
 
 document.getElementById('clearGraph').addEventListener('click', function () {
-    console.log('Button clicked!');
+    console.log('Clear Button clicked!');
     cy.remove(cy.elements());
 });
 
@@ -142,7 +139,7 @@ function addLinkedTable(n, arr) {
     document.getElementById('lnkText').style.visibility = 'visible';
     var myTableDiv = document.getElementById("linkedRepTable");
 
-    if (myTableDiv.hasChildNodes()) {
+    while (myTableDiv.hasChildNodes()) {
         myTableDiv.removeChild(myTableDiv.childNodes[0]);
     }
 
@@ -169,7 +166,7 @@ function addLinkedTable(n, arr) {
         th.appendChild(document.createTextNode(String.fromCharCode(96 + i + 1)));
         tr.appendChild(th);
         tableBody.appendChild(tr);
-        
+
         var data = "";
         for (var j = 0; j < n; j++) {
             if (arr[i][j] != 0) {
@@ -179,6 +176,164 @@ function addLinkedTable(n, arr) {
         var td = document.createElement('TD');
         td.appendChild(document.createTextNode(data));
         tr.appendChild(td);
+    }
+    myTableDiv.appendChild(table);
+}
+
+function warshall(n, arr) {
+    document.getElementById('warText').style.visibility = 'visible';
+
+    var myTableDiv = document.getElementById("warMatTable");
+    while (myTableDiv.hasChildNodes()) {
+        myTableDiv.removeChild(myTableDiv.childNodes[0]);
+    }
+
+    var arr_temp = [];
+    for (var i = 0; i < n; i++) {
+        arr_temp[i] = [];
+        for (var j = 0; j < n; j++) {
+            if (arr[i][j] == "0") {
+                arr_temp[i][j] = 9999;
+            } else
+                arr_temp[i][j] = parseInt(arr[i][j]);
+        }
+    }
+    // console.log("arr: ", arr);
+
+    for (var k = 0; k < n; k++) {
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < n; j++) {
+                if (arr_temp[i][k] + arr_temp[k][j] < arr_temp[i][j]) {
+                    arr_temp[i][j] = arr_temp[i][k] + arr_temp[k][j];
+                }
+            }
+        }
+        warTableDraw(n, arr_temp, k + 1);
+    }
+    console.log("Warshall's matrix :\n", arr_temp);
+}
+
+function warTableDraw(n, arr, k) {
+    var myTableDiv = document.getElementById("warMatTable");
+    var table = document.createElement('TABLE');
+    table.className = 'table table-hover';
+
+    // caption
+    var caption = table.createCaption();
+    if (n != k)
+        caption.innerText = 'Warshall\'s Matrix after ' + k + ' iteration';
+    else 
+        caption.innerText = 'Warshall\'s Matrix after final iteration (shortest path or path matrix)';
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode('#'));
+    tr.appendChild(th);
+    for (var j = 0; j < n; j++) {
+        var th = document.createElement('TH');
+        th.appendChild(document.createTextNode("To " + String.fromCharCode(96 + j + 1)));
+        tr.appendChild(th);
+    }
+
+    for (var i = 0; i < n; i++) {
+        var tr = document.createElement('TR');
+        var th = document.createElement('TH');
+        th.appendChild(document.createTextNode("From " + String.fromCharCode(96 + i + 1)));
+        tr.appendChild(th);
+        tableBody.appendChild(tr);
+
+        for (var j = 0; j < n; j++) {
+            var td = document.createElement('TD');
+            if (arr[i][j] == 9999) {
+                td.appendChild(document.createTextNode("∞"));
+            } else
+                td.appendChild(document.createTextNode(arr[i][j]));
+            tr.appendChild(td);
+        }
+    }
+    myTableDiv.appendChild(table);
+}
+
+function pathMatrix(n, arr) {
+    document.getElementById('pathText').style.visibility = 'visible';
+
+    var myTableDiv = document.getElementById("pathMatTable");
+    while (myTableDiv.hasChildNodes()) {
+        myTableDiv.removeChild(myTableDiv.childNodes[0]);
+    }
+
+    console.log("arr : ", arr);
+    var arr_one = [];
+    var arr_two = [];
+    for (var i = 0; i < n; i++) {
+        arr_one[i] = [];
+        arr_two[i] = [];
+        for (var j = 0; j < n; j++) {
+            arr_one[i][j] = parseInt(arr[i][j]);
+            arr[i][j] = parseInt(arr[i][j]);
+        }
+    }
+    console.log("arr_one : ", arr_one);
+
+    for (var loop = 0; loop < n; loop++) {
+        for (var i = 0; i < n; i++) {
+            for (var j = 0; j < n; j++) {
+                var sum = 0;
+                for (var k = 0; k < n; k++) {
+                    sum = sum + arr_one[i][k] * arr[k][j];
+                }
+                arr_two[i][j] = sum;
+            }
+        }
+        printTableMatrix(n, arr_two, loop + 1);
+    }
+}
+
+function printTableMatrix(n, arr_temp, loop) {
+    var myTableDiv = document.getElementById("pathMatTable");
+    var table = document.createElement('TABLE');
+    table.className = 'table table-hover';
+
+    // caption
+    var caption = table.createCaption();
+    if (n != loop)
+        caption.innerText = 'Path Matrix after ' + loop + ' iteration';
+    else 
+        caption.innerText = 'Path Matrix after final iteration';
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode('#'));
+    tr.appendChild(th);
+    for (var j = 0; j < n; j++) {
+        var th = document.createElement('TH');
+        th.appendChild(document.createTextNode("To " + String.fromCharCode(96 + j + 1)));
+        tr.appendChild(th);
+    }
+
+    for (var i = 0; i < n; i++) {
+        var tr = document.createElement('TR');
+        var th = document.createElement('TH');
+        th.appendChild(document.createTextNode("From " + String.fromCharCode(96 + i + 1)));
+        tr.appendChild(th);
+        tableBody.appendChild(tr);
+
+        for (var j = 0; j < n; j++) {
+            var td = document.createElement('TD');
+            if (arr_temp[i][j] == 9999) {
+                td.appendChild(document.createTextNode("∞"));
+            } else
+                td.appendChild(document.createTextNode(arr_temp[i][j]));
+            tr.appendChild(td);
+        }
     }
     myTableDiv.appendChild(table);
 }
